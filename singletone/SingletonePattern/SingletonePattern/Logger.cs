cec88f136d1e69cabc32688fc9f46c5c3583d7a0
yellalena <yellalena@gmail.com>
2023-03-19 12:55:26 +0100

    singleton pattern

---
+++ singletone/SingletonePattern/SingletonePattern/Logger.cs
@@ -0,0 +1,9 @@
+﻿namespace SingletonPattern
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _loggerInstance = new(() => new Logger());
+        public static Logger Instance { get { return _loggerInstance.Value; } }
+        private Logger() { }
+    }
+}
---
+++ singletone/SingletonePattern/SingletonePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using SingletonPattern;
+
+class Singleton
+{
+    public static void Main()
+    {
+        Thread t1 = new(() =>
+        {
+            var logger = Logger.Instance;
+            Console.WriteLine($"Thread 1, logger instance: {logger.GetHashCode()}");
+        });
+
+        Thread t2 = new(() =>
+        {
+            var logger = Logger.Instance;
+            Console.WriteLine($"Thread 2, logger instance: {logger.GetHashCode()}");
+        });
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+    }
+}
